d0c9c3711b848fd4cc8e98c9d8ae27a856944d4f
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-09 20:54:54 +0200

    Create freedom-bootstrap-one-liner.sh

---
+++ freedom-internet/freedom-bootstrap-one-liner.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -L https://raw.githubusercontent.com/henrydenhengst/mydesktop/main/freedom-internet/freedom-bootstrap-pro.sh \
+  -o /tmp/freedom-bootstrap-pro.sh && \
+chmod +x /tmp/freedom-bootstrap-pro.sh && \
+/tmp/freedom-bootstrap-pro.sh
