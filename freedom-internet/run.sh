86ebe9bb18e6bb1431256b9da962eb9012682701
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-09 21:13:41 +0200

    Update run.sh

--- freedom-internet/run.sh
+++ freedom-internet/run.sh
@@ -1,1 +1,2 @@
-sh -c "$(curl -sL https://raw.githubusercontent.com/henrydenhengst/mydesktop/main/freedom-internet/freedom-bootstrap-pro.sh)"
+scp freedom-bootstrap-pro.sh root@opnsense:/tmp/ && \
+ssh root@opnsense "chmod +x /tmp/freedom-bootstrap-pro.sh && /tmp/freedom-bootstrap-pro.sh && rm -f /tmp/freedom-bootstrap-pro.sh"
