ad08bd2e142ed0910eb8f8744d4c79ec2f4e6ec5
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-15 11:53:20 +0200

    Update freedom-bootstrap-pro.sh

--- freedom-internet/freedom-bootstrap-pro.sh
+++ freedom-internet/freedom-bootstrap-pro.sh
@@ -1,169 +1,158 @@
 #!/bin/sh
 set -e
 
-###############################################################################
-# Freedom Internet + TV PRO (FIXED BOOTSTRAP)
-###############################################################################
+#########################################
+# CONFIG
+#########################################
 
-LOG="/var/log/freedom-bootstrap.log"
-CONFIG="/conf/config.xml"
-BACKUP="/conf/config.xml.bak.$(date +%s)"
-TMP="/tmp/config.xml.new"
+OPNSENSE_HOST="https://127.0.0.1"
+API_KEY="YOUR_API_KEY"
+API_SECRET="YOUR_API_SECRET"
 
 WAN_IF="igb0"
 LAN_IF="igb1"
 
+VLAN6_TAG="6"
+VLAN4_TAG="4"
+
 log() {
-  echo "$(date '+%Y-%m-%d %H:%M:%S') $1" | tee -a $LOG
+  echo "[*] $1"
 }
 
-rollback() {
-  log "[!] FOUT - rollback"
-  cp $BACKUP $CONFIG
-  configctl system reload
-  exit 1
+api() {
+  curl -sk -u "${API_KEY}:${API_SECRET}" \
+    -H "Content-Type: application/json" \
+    "$@"
 }
 
-trap rollback ERR
+#########################################
+# 1. VLANs aanmaken
+#########################################
 
-log "[*] Start bootstrap"
+log "VLAN6 aanmaken (Internet)"
+api -X POST "${OPNSENSE_HOST}/api/interfaces/vlan/add" \
+  -d "{
+    \"parent\": \"${WAN_IF}\",
+    \"tag\": ${VLAN6_TAG},
+    \"descr\": \"WAN_VLAN6\"
+  }"
 
-cp $CONFIG $BACKUP
-cp $CONFIG $TMP
+log "VLAN4 aanmaken (IPTV)"
+api -X POST "${OPNSENSE_HOST}/api/interfaces/vlan/add" \
+  -d "{
+    \"parent\": \"${WAN_IF}\",
+    \"tag\": ${VLAN4_TAG},
+    \"descr\": \"WAN_VLAN4_TV\"
+  }"
 
-###############################################################################
-# VLAN 6 (Internet)
-###############################################################################
-if ! grep -q "<tag>6</tag>" $TMP; then
-  log "[*] VLAN6 toevoegen"
-  sed -i '' "/<vlans>/a\\
-    <vlan>\\
-      <if>${WAN_IF}</if>\\
-      <tag>6</tag>\\
-      <descr>WAN_VLAN6</descr>\\
-    </vlan>" $TMP
-fi
+#########################################
+# 2. Interfaces toewijzen
+#########################################
 
-###############################################################################
-# VLAN 4 (IPTV)
-###############################################################################
-if ! grep -q "<tag>4</tag>" $TMP; then
-  log "[*] VLAN4 toevoegen"
-  sed -i '' "/<vlans>/a\\
-    <vlan>\\
-      <if>${WAN_IF}</if>\\
-      <tag>4</tag>\\
-      <descr>WAN_VLAN4_TV</descr>\\
-    </vlan>" $TMP
-fi
+log "Interfaces toewijzen"
 
-###############################################################################
-# PPPoE op VLAN6
-###############################################################################
-if ! grep -q "freedom_pppoe" $TMP; then
-  log "[*] PPPoE toevoegen"
+api -X POST "${OPNSENSE_HOST}/api/interfaces/assign" \
+  -d '{"device":"pppoe0","enable":true}'
 
-  sed -i '' "/<opnsense>/i\\
-  <ppps>\\
-    <ppp>\\
-      <ptpid>0</ptpid>\\
-      <type>pppoe</type>\\
-      <if>${WAN_IF}_vlan6</if>\\
-      <username>fake@freedom.nl</username>\\
-      <password>1234</password>\\
-      <descr>freedom_pppoe</descr>\\
-    </ppp>\\
-  </ppps>" $TMP
-fi
+#########################################
+# 3. PPPoE op VLAN6
+#########################################
 
-###############################################################################
-# WAN -> PPPoE (belangrijk)
-###############################################################################
-sed -i '' "s|<if>${WAN_IF}</if>|<if>pppoe0</if>|" $TMP
+log "PPPoE configureren"
 
-###############################################################################
-# MTU FIX (geen MSS clamp)
-###############################################################################
-if ! grep -q "<mtu>1510</mtu>" $TMP; then
-  log "[*] MTU instellen"
-  sed -i '' "/<wan>/a\\
-      <mtu>1510</mtu>" $TMP
-fi
+api -X POST "${OPNSENSE_HOST}/api/ppps/settings/set" \
+  -d "{
+    \"ppps\": {
+      \"ppp\": [
+        {
+          \"type\": \"pppoe\",
+          \"if\": \"${WAN_IF}_vlan6\",
+          \"username\": \"fake@freedom.nl\",
+          \"password\": \"1234\",
+          \"descr\": \"Freedom PPPoE\"
+        }
+      ]
+    }
+  }"
 
-###############################################################################
-# IPv6 (PD /48)
-###############################################################################
-if ! grep -q "dhcp6" $TMP; then
-  log "[*] IPv6 toevoegen"
-  sed -i '' "/<wan>/a\\
-      <ipaddrv6>dhcp6</ipaddrv6>\\
-      <dhcp6-ia-pd-len>48</dhcp6-ia-pd-len>" $TMP
-fi
+#########################################
+# 4. WAN instellingen (MTU FIX)
+#########################################
 
-###############################################################################
-# VLAN4 DHCP CLIENT OP WAN (BELANGRIJK FIX)
-###############################################################################
-if ! grep -q "igb0_vlan4_dhcp" $TMP; then
-  log "[*] VLAN4 DHCP client toevoegen"
+log "WAN MTU instellen"
 
-  sed -i '' "/<opnsense>/i\\
-  <dhcpd>\\
-    <wan_vlan4>\\
-      <enable>1</enable>\\
-      <interface>${WAN_IF}_vlan4</interface>\\
-    </wan_vlan4>\\
-  </dhcpd>" $TMP
-fi
+api -X POST "${OPNSENSE_HOST}/api/interfaces/settings/set" \
+  -d "{
+    \"interfaces\": {
+      \"wan\": {
+        \"mtu\": 1510,
+        \"ipaddrv6\": \"dhcp6\",
+        \"dhcp6-ia-pd-len\": 48
+      }
+    }
+  }"
 
-###############################################################################
-# IPTV LAN subnet (simpel + stabiel)
-###############################################################################
-TV_NET="192.168.100"
+#########################################
+# 5. VLAN4 DHCP (IPTV WAN side fix)
+#########################################
 
-if ! grep -q "192.168.100" $TMP; then
-  log "[*] IPTV LAN DHCP"
+log "VLAN4 DHCP client activeren"
 
-  sed -i '' "/<dhcpd>/a\\
-    <lan>\\
-      <enable>1</enable>\\
-      <range>${TV_NET}.10 ${TV_NET}.200</range>\\
-      <interface>${LAN_IF}</interface>\\
-    </lan>" $TMP
-fi
+api -X POST "${OPNSENSE_HOST}/api/interfaces/settings/set" \
+  -d "{
+    \"interfaces\": {
+      \"wan_vlan4\": {
+        \"if\": \"${WAN_IF}_vlan4\",
+        \"ipv4\": \"dhcp\"
+      }
+    }
+  }"
 
-###############################################################################
-# IGMP PROXY (basic correct model)
-###############################################################################
-if ! grep -q "igmpproxy" $TMP; then
-  log "[*] IGMP proxy"
+#########################################
+# 6. LAN DHCP IPTV subnet
+#########################################
 
-  sed -i '' "/<opnsense>/i\\
-  <igmpproxy>\\
-    <enable>1</enable>\\
-    <interfaces>\\
-      <upstream>${WAN_IF}_vlan4</upstream>\\
-      <downstream>${LAN_IF}</downstream>\\
-    </interfaces>\\
-  </igmpproxy>" $TMP
-fi
+log "LAN DHCP instellen"
 
-###############################################################################
-# VALIDATE
-###############################################################################
-if command -v xmllint >/dev/null 2>&1; then
-  log "[*] XML check"
-  xmllint --noout $TMP || rollback
-fi
+api -X POST "${OPNSENSE_HOST}/api/dhcpd/settings/set" \
+  -d "{
+    \"lan\": {
+      \"enable\": 1,
+      \"range\": \"192.168.100.10 192.168.100.200\"
+    }
+  }"
 
-###############################################################################
-# APPLY
-###############################################################################
-cp $TMP $CONFIG
+#########################################
+# 7. IGMP proxy (correct model)
+#########################################
 
-log "[*] Reload config"
-configctl system reload
-configctl interface reload all
+log "IGMP proxy instellen"
 
-log "[*] Reboot in 5s"
-sleep 5
-reboot
+api -X POST "${OPNSENSE_HOST}/api/igmpproxy/settings/set" \
+  -d "{
+    \"general\": {
+      \"enable\": 1
+    },
+    \"igmp\": {
+      \"interfaces\": [
+        {
+          \"type\": \"upstream\",
+          \"name\": \"${WAN_IF}_vlan4\"
+        },
+        {
+          \"type\": \"downstream\",
+          \"name\": \"${LAN_IF}\"
+        }
+      ]
+    }
+  }"
+
+#########################################
+# 8. Apply
+#########################################
+
+log "Apply config"
+
+api -X POST "${OPNSENSE_HOST}/api/core/firmware/reconfigure"
+
+log "Klaar. Reboot aanbevolen."
