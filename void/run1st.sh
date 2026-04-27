d3cab6ab6f3e2566ae020de71c6f0198666604b6
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-27 22:58:10 +0200

    Update run1st.sh

--- void/run1st.sh
+++ void/run1st.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "--- Start post-installatiescript ---"
+
+# 1. Installeer de benodigde Ansible collecties
+echo "Bezig met installeren van Ansible collecties..."
 ansible-galaxy collection install community.general
+
+# 2. Start het Playbook
+echo "Start Ansible Playbook op localhost..."
 ansible-playbook -K post-install.yml -c local
+
+echo "--- Klaar! Vergeet niet te rebooten voor de nieuwe kernel en services. ---"
