ee86b13df5d2c78bd231837a4d4e34e0fde2443b
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-28 10:42:07 +0200

    Update install-for-uefi.sh

--- void/install-for-uefi.sh
+++ void/install-for-uefi.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 set -e
 
-# --- CONFIGURATIE ---
-DISK="/dev/sda"
+# --- AUTOMATISCHE CONFIGURATIE ---
+# Zoekt de grootste schijf (disk) en negeert de live-usb/loop devices
+DISK=$(lsblk -dpno NAME,TYPE,SIZE | grep 'disk' | sort -rnk3 | head -n1 | awk '{print $1}')
+
 HOSTNAME="void-uefi"
 USERNAME="voiduser"
-if [[ $DISK == *"nvme"* ]]; then P="p"; else P=""; fi
 
-echo "--- START UEFI INSTALLATIE ---"
+# Check of er een schijf gevonden is
+if [ -z "$DISK" ]; then
+    echo "FOUT: Geen geschikte harde schijf gevonden!"
+    exit 1
+fi
 
-# 1. Partitioneren
+# Bepaal de partitie-suffix (leeg voor /dev/sda, 'p' voor /dev/nvme0n1)
+if [[ $DISK == *"nvme"* ]] || [[ $DISK == *"mmcblk"* ]]; then 
+    P="p"
+else 
+    P=""
+fi
+
+echo "--- START UEFI INSTALLATIE OP $DISK ---"
+
+# 1. Partitioneren (GPT)
+echo "Partitioneren van $DISK..."
 sfdisk "$DISK" <<EOF
 label: gpt
 device: $DISK
@@ -20,8 +35,10 @@ ${DISK}${P}2 : start=2099200, size=+, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
 # 2. Formatteren & Mounten
+echo "Formatteren van partities..."
 mkfs.vfat "${DISK}${P}1"
 mkfs.ext4 -F "${DISK}${P}2"
+
 mount "${DISK}${P}2" /mnt
 mkdir -p /mnt/boot/efi
 mount "${DISK}${P}1" /mnt/boot/efi
@@ -35,6 +52,7 @@ XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R https://repo-default.voidlinux.org/c
     base-system grub-x86_64-efi vim git ansible pciutils usbutils sudo xtools dbus ca-certificates
 
 # 4. Fstab & Chroot
+echo "Configureren van het systeem..."
 echo "${DISK}${P}2 / ext4 defaults 0 1" > /mnt/etc/fstab
 echo "${DISK}${P}1 /boot/efi vfat defaults 0 2" >> /mnt/etc/fstab
 
@@ -49,9 +67,11 @@ echo "$USERNAME:voidlinux" | chpasswd
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
 
+# Bootloader installatie
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void"
 update-grub
 EOF
 
 umount -R /mnt
-echo "KLAAR! Reboot en start je Ansible playbook."
+echo "--- KLAAR! ---"
+echo "Je kunt nu rebooten. Vergeet de USB-sticks te verwijderen."
