2df4293c1d0a6c286120366294bae62aadef452a
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-28 10:05:01 +0200

    Update install-for-uefi.sh

--- void/install-for-uefi.sh
+++ void/install-for-uefi.sh
@@ -2,48 +2,42 @@
 set -e
 
 # --- CONFIGURATIE ---
-DISK="/dev/sda"  # Pas dit aan (bijv. /dev/sda of /dev/nvme0n1)
-HOSTNAME="void-efi"
+DISK="/dev/sda"
+HOSTNAME="void-uefi"
 USERNAME="voiduser"
-
-# Detecteer partitie-naming (nvme0n1p1 vs sda1)
 if [[ $DISK == *"nvme"* ]]; then P="p"; else P=""; fi
 
-echo "Start UEFI installatie op $DISK..."
+echo "--- START UEFI INSTALLATIE ---"
 
-# 1. Partitioneren (GPT: EFI + Root)
-echo "Partitioneren..."
+# 1. Partitioneren
 sfdisk "$DISK" <<EOF
 label: gpt
 device: $DISK
 unit: sectors
 
-${DISK}${P}1 : start=2048, size=1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-${DISK}${P}2 : start=1050624, size=+, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+${DISK}${P}1 : start=2048, size=2097152, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+${DISK}${P}2 : start=2099200, size=+, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
-# 2. Formatteren
-echo "Formatteren..."
+# 2. Formatteren & Mounten
 mkfs.vfat "${DISK}${P}1"
 mkfs.ext4 -F "${DISK}${P}2"
-
 mount "${DISK}${P}2" /mnt
 mkdir -p /mnt/boot/efi
 mount "${DISK}${P}1" /mnt/boot/efi
 
-# 3. Base system installeren
-echo "Pakketten installeren..."
+# 3. Base system + ALLE verzochte tools installeren
+echo "Installeren van base-system en beheer-tools..."
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
-XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R https://repo-default.voidlinux.org/current \
-    base-system grub-x86_64-efi vim git ansible
 
-# 4. Fstab genereren (Heel simpel voor ext4)
+XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R https://repo-default.voidlinux.org/current \
+    base-system grub-x86_64-efi vim git ansible pciutils usbutils sudo xtools dbus ca-certificates
+
+# 4. Fstab & Chroot
 echo "${DISK}${P}2 / ext4 defaults 0 1" > /mnt/etc/fstab
 echo "${DISK}${P}1 /boot/efi vfat defaults 0 2" >> /mnt/etc/fstab
 
-# 5. Configureren (Chroot)
-echo "Systeem configureren..."
 chroot /mnt /bin/bash <<EOF
 echo "$HOSTNAME" > /etc/hostname
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -51,9 +45,13 @@ useradd -m -G wheel,users -s /bin/bash $USERNAME
 echo "root:voidlinux" | chpasswd
 echo "$USERNAME:voidlinux" | chpasswd
 
+# Sudo & Dbus voorbereiding voor Ansible
+echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
+ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void"
 update-grub
 EOF
 
 umount -R /mnt
-echo "Klaar! Reboot nu."
+echo "KLAAR! Reboot en start je Ansible playbook."
