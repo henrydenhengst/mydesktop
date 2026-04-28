75b4da11fb03482b773b56db7e32b315077a91ec
Henry den Hengst <henrydenhengst@gmail.com>
2026-04-28 10:04:32 +0200

    Update install-for-bios.sh

--- void/install-for-bios.sh
+++ void/install-for-bios.sh
@@ -2,12 +2,11 @@
 set -e
 
 # --- CONFIGURATIE ---
-# Tip: Gebruik $(lsblk -dpno NAME | grep -vE 'loop|boot' | head -n1) voor automatische detectie
 DISK="/dev/sda"  
 HOSTNAME="void-bios"
 USERNAME="voiduser"
 
-echo "Start BIOS installatie op $DISK..."
+echo "--- START BIOS INSTALLATIE ---"
 
 # 1. Partitioneren
 echo "Partitioneren..."
@@ -19,42 +18,33 @@ unit: sectors
 ${DISK}1 : start=2048, type=83, bootable
 EOF
 
-# 2. Formatteren
-echo "Formatteren..."
+# 2. Formatteren & Mounten
 mkfs.ext4 -F "${DISK}1"
 mount "${DISK}1" /mnt
 
-# 3. Base system installeren
-# BELANGRIJK: Hier voegen we de extra tools toe voor jouw Ansible playbook
-echo "Pakketten installeren (inclusief Ansible benodigdheden)..."
+# 3. Base system + ALLE verzochte tools installeren
+echo "Installeren van base-system en beheer-tools..."
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 
 XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R https://repo-default.voidlinux.org/current \
-    base-system grub vim git ansible pciutils usbutils sudo xtools
+    base-system grub vim git ansible pciutils usbutils sudo xtools dbus ca-certificates
 
 # 4. Configureren (Chroot)
-echo "Systeem configureren..."
 chroot /mnt /bin/bash <<EOF
 echo "$HOSTNAME" > /etc/hostname
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/rc.conf
-
-# Gebruiker aanmaken en wachtwoorden instellen
 useradd -m -G wheel,users -s /bin/bash $USERNAME
 echo "root:voidlinux" | chpasswd
 echo "$USERNAME:voidlinux" | chpasswd
 
-# Sudo rechten alvast aanzetten zodat Ansible direct aan de slag kan
+# Sudo & Dbus voorbereiding voor Ansible
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
+ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
 
-# Bootloader
 grub-install "$DISK"
 update-grub
 EOF
 
 umount -R /mnt
-echo "--- Basisinstallatie voltooid! ---"
-echo "1. Reboot"
-echo "2. Log in als $USERNAME"
-echo "3. Run de post install
+echo "KLAAR! Reboot en start je Ansible playbook."
