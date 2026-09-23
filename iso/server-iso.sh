25d31300078aa73855ff5e16874b05b449ebc62b
Henry den Hengst <henrydenhengst@gmail.com>
2026-09-23 19:27:26 +0200

    Create iso/server-iso.sh

---
+++ iso/server-iso.sh
@@ -0,0 +1,7 @@
+wget -c https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.2-x86_64-minimal.iso
+
+wget -c https://mirror.nl.leaseweb.net/opensuse/distribution/leap/16.0/offline/Leap-16.0-online-installer-x86_64-Build178.27.install.iso
+
+wget -c https://repo.almalinux.org/almalinux/9/isos/x86_64/AlmaLinux-9.8-x86_64-minimal.iso
+
+wget -c https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.8-x86_64-minimal.iso
